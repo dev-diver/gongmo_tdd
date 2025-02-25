e702b9d0a3faf7fba30154a2bc930fd6e0761299
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2025-02-25 21:30:05 +0900

    testcontainers

--- cmd/httpserver/account_server_test.go
+++ cmd/httpserver/account_server_test.go
@@ -1,14 +1,39 @@
 package main_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/dev-diver/gongmo/domain"
 	"github.com/dev-diver/gongmo/driver"
 	"github.com/dev-diver/gongmo/specifications"
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestAccountServer(t *testing.T) {
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		FromDockerfile: testcontainers.FromDockerfile{
+			Context:    "../../.",
+			Dockerfile: "./cmd/httpserver/Dockerfile",
+			// set to false if you want less spam, but this is helpful if you're having troubles
+			PrintBuildLog: true,
+		},
+		ExposedPorts: []string{"8080:8080"},
+		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	assert.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, container.Terminate(ctx))
+	})
+
 	driver := driver.Driver{BaseURL: "http://localhost:8080"}
-	specifications.AccountRetrievalSpec(t, driver, domain.AccountId("1"), 100, nil)
+	specifications.AccountRetrievalSpec(t, driver, domain.AccountId("1"), 200, nil)
 }
